766c2384c56f3bba6aaf4bfa250ccc9a9d2c6d2c
Дмитрий Парфенов <parfyonovdv@gmail.com>
2025-01-31 23:09:27 +0300

    hw

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String title;
+    private String author;
+    private int yearOfPublication;
+
+    public Book(String title, String author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Book book = new Book("Мартин Тден", "Джек Лондон", 1909);
+        System.out.println("book.title = " + book.getTitle());
+        System.out.println("book.author = " + book.getAuthor());
+        System.out.println("book.yearOfPublication = " + book.getYearOfPublication());
+        book.setYearOfPublication(2025);
+        System.out.println("book.getYearOfPublication() = " + book.getYearOfPublication());
+
+        Author author = new Author("Джек", "Лондон");
+        System.out.println("author.firstName = " + author.getFirstName());
+        System.out.println("author.lastName = " + author.getLastName());
     }
 }
